e7a44b28e61b294bebff19e618cec17f7526516b
Roman2005p <59954184+Roman2005p@users.noreply.github.com>
2020-10-30 13:42:29 +0600

    Add files via upload

---
+++ src/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle extends Shape {
+
+    private int width;
+    private int height;
+
+
+    private Rectangle(){
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int calcSquare ( ){
+        return this.width * this.height;
+
+    }
+}
